ae192ab82eabc6114e1dafef4ec135e140819a39
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-17 08:47:51 +0100

    sdv

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._11
 {
@@ -6,7 +7,7 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            return str.SequenceEqual(str.Reverse());
         } 
     }
 }
